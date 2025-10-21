bba8550adfecaec49b0a9879592564967743f675
Sekvojak <dominikkontrik123@gmail.com>
2025-10-21 11:54:38 +0200

    dalsia verzia

---
+++ cviko6practise/socket_cl_v2.cpp
@@ -0,0 +1,223 @@
+//***************************************************************************
+//
+// Program example for subject Operating Systems
+//
+// Petr Olivka, Dept. of Computer Science, petr.olivka@vsb.cz, 2021
+//
+// Example of socket server/client.
+//
+// This program is example of socket client.
+// The mandatory arguments of program is IP adress or name of server and
+// a port number.
+//
+//***************************************************************************
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdarg.h>
+#include <poll.h>
+#include <sys/socket.h>
+#include <sys/param.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netdb.h>
+#include <ctime>
+
+#define STR_CLOSE               "close"
+
+//***************************************************************************
+// log messages
+
+#define LOG_ERROR               0       // errors
+#define LOG_INFO                1       // information and notifications
+#define LOG_DEBUG               2       // debug messages
+
+// debug flag
+int g_debug = LOG_INFO;
+
+void log_msg( int t_log_level, const char *t_form, ... )
+{
+    const char *out_fmt[] = {
+            "ERR: (%d-%s) %s\n",
+            "INF: %s\n",
+            "DEB: %s\n" };
+
+    if ( t_log_level && t_log_level > g_debug ) return;
+
+    char l_buf[ 1024 ];
+    va_list l_arg;
+    va_start( l_arg, t_form );
+    vsprintf( l_buf, t_form, l_arg );
+    va_end( l_arg );
+
+    switch ( t_log_level )
+    {
+    case LOG_INFO:
+    case LOG_DEBUG:
+        fprintf( stdout, out_fmt[ t_log_level ], l_buf );
+        break;
+
+    case LOG_ERROR:
+        fprintf( stderr, out_fmt[ t_log_level ], errno, strerror( errno ), l_buf );
+        break;
+    }
+}
+
+//***************************************************************************
+// help
+
+void help( int t_narg, char **t_args )
+{
+    if ( t_narg <= 1 || !strcmp( t_args[ 1 ], "-h" ) )
+    {
+        printf(
+            "\n"
+            "  Socket client example.\n"
+            "\n"
+            "  Use: %s [-h -d] ip_or_name port_number\n"
+            "\n"
+            "    -d  debug mode \n"
+            "    -h  this help\n"
+            "\n", t_args[ 0 ] );
+
+        exit( 0 );
+    }
+
+    if ( !strcmp( t_args[ 1 ], "-d" ) )
+        g_debug = LOG_DEBUG;
+}
+
+//***************************************************************************
+
+int main( int t_narg, char **t_args )
+{   
+    srand(time(NULL));
+    // 1️⃣ Spracovanie argumentov
+    if ( t_narg <= 2 ) help( t_narg, t_args );
+    // ➡️ Program očakáva dva povinné argumenty:
+        // adresu servera (ip_or_name),
+        // číslo portu (port_number).
+    int l_port = 0;
+    char *l_host = nullptr;
+
+    if (t_narg < 4)
+    {
+        log_msg(LOG_ERROR, "Malo argumentov");
+        exit(1);
+    }
+    
+
+    // parsing arguments
+    for ( int i = 1; i < t_narg; i++ )
+    {
+        if ( !strcmp( t_args[ i ], "-d" ) )
+            g_debug = LOG_DEBUG;
+
+        if ( !strcmp( t_args[ i ], "-h" ) )
+            help( t_narg, t_args );
+
+        if ( *t_args[ i ] != '-' )
+        {
+            if ( !l_host )
+                l_host = t_args[ i ];
+            else if ( !l_port )
+                l_port = atoi( t_args[ i ] );
+        }
+    }
+    // ➡️ Ak nezada IP alebo port → vypíše chybu a skončí.
+    if ( !l_host || !l_port )
+    {
+        log_msg( LOG_INFO, "Host or port is missing!" );
+        help( t_narg, t_args );
+        exit( 1 );
+    }
+
+    log_msg( LOG_INFO, "Connection to '%s':%d.", l_host, l_port );
+
+
+    addrinfo l_ai_req, *l_ai_ans;
+    bzero( &l_ai_req, sizeof( l_ai_req ) ); // vyplni strukturu aby neboli nahodne hodnoty, mi vypiseme len tie co ma zaujimaju
+    l_ai_req.ai_family = AF_INET; // AF_INET → IPv4 adresu,
+    l_ai_req.ai_socktype = SOCK_STREAM; // SOCK_STREAM → TCP socket.
+
+    // výstup (odpoveď)
+    int l_get_ai = getaddrinfo( l_host, nullptr, &l_ai_req, &l_ai_ans );
+    if ( l_get_ai )
+    {
+        log_msg( LOG_ERROR, "Unknown host name!" );
+        exit( 1 );
+    }
+
+    sockaddr_in l_cl_addr =  *( sockaddr_in * ) l_ai_ans->ai_addr; // Zoberie adresu zo štruktúry addrinfo, ktorú nám getaddrinfo() vrátilo.
+    l_cl_addr.sin_port = htons( l_port );
+    freeaddrinfo( l_ai_ans );
+
+    // socket creation
+    int l_sock_server = socket( AF_INET, SOCK_STREAM, 0 );
+    if ( l_sock_server == -1 )
+    {
+        log_msg( LOG_ERROR, "Unable to create socket.");
+        exit( 1 );
+    }
+
+    // connect to server
+    if ( connect( l_sock_server, ( sockaddr * ) &l_cl_addr, sizeof( l_cl_addr ) ) < 0 )
+    {
+        log_msg( LOG_ERROR, "Unable to connect server." );
+        exit( 1 );
+    }
+
+    uint l_lsa = sizeof( l_cl_addr );
+    // my IP
+    getsockname( l_sock_server, ( sockaddr * ) &l_cl_addr, &l_lsa );
+    log_msg( LOG_INFO, "My IP: '%s'  port: %d",
+             inet_ntoa( l_cl_addr.sin_addr ), ntohs( l_cl_addr.sin_port ) );
+    // server IP
+    getpeername( l_sock_server, ( sockaddr * ) &l_cl_addr, &l_lsa );
+    log_msg( LOG_INFO, "Server IP: '%s'  port: %d",
+             inet_ntoa( l_cl_addr.sin_addr ), ntohs( l_cl_addr.sin_port ) );
+
+    log_msg( LOG_INFO, "Enter 'close' to close application." );
+
+
+    char resolution[64];
+    snprintf(resolution, sizeof(resolution), "%s!", t_args[3]); // 3 argument
+    log_msg(LOG_INFO, "Requested resize: '%s'", resolution);
+    write(l_sock_server, resolution, strlen(resolution));
+    
+    
+    // image.img ulozenie 
+    int fd = open("image.img", O_WRONLY );
+    if (!fd)
+    {
+        log_msg(LOG_ERROR, "Problem s otvorenim suboru!");
+        close(l_sock_server);
+        exit(1);
+    }
+
+    char buffer[4086];
+    int len;
+    while ((len = read(l_sock_server, buffer, sizeof(buffer))) > 0)
+    {
+        write(fd, buffer, len);
+    }
+
+    log_msg(LOG_INFO, "Subor ulozeny ako umage.img");
+    close(fd);
+    execlp("display", "display", "image.img", NULL);
+    
+    
+    
+
+
+    // close socket
+    close( l_sock_server );
+
+    return 0;
+  }
